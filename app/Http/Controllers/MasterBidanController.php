83123f53f99931fb5a141ddc12cbdf28eba64736
faharmuharram <146865706+faharmuharram@users.noreply.github.com>
2023-10-08 01:29:26 +0700

    Menambahkan view master bidan dan add master bidan, namun belum dapat save

--- app/Http/Controllers/MasterBidanController.php
+++ app/Http/Controllers/MasterBidanController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MBidan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MasterBidanController extends Controller
 {
     public function view()
     {
-        return view('m_bidan');
+        $datas = DB::select('Select * From m_bidan');
+        // echo "<pre>";
+        // die(var_dump($data));
+
+        return view('m_bidan', compact('datas'));
+    }
+    public function add()
+    {
+        return view('m_bidan_add');
     }
 }
